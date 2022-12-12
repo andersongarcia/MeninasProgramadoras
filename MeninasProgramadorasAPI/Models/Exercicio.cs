445393af53fd1adfacf0b3504d0fc95753f2d419
Anderson Canale Garcia <andersongarcia@gmail.com>
2022-12-12 07:41:55 -0300

    Início Exercício

---
+++ MeninasProgramadorasAPI/Models/Exercicio.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeninasProgramadorasAPI.Models;
+
+public class Exercicio
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    public Avaliacao Avaliacao { get; set; }
+    public TipoDeEvento TipoDeExercicio { get; set; }
+    public int? NumeroExercicio { get; set; }  // 1 para Exercício 1, 2 para Exercício 2, etc
+    public int Total { get; set; } = 0;
+    public int Resolvidos { get; set; } = 0;
+}
---
+++ MeninasProgramadorasAPI/Models/TipoDeExercicio.cs
@@ -0,0 +1,6 @@
+﻿namespace MeninasProgramadorasAPI.Models;
+
+public enum TipoDeExercicio
+{
+    Exercicio, AvaliacaoFinal, Recuperacao
+}
